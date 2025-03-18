aa39df5e9ac2a30868eb857ad073ecb43b857cd6
adi-baranovsky <adi.baranovsky@gmail.com>
2025-03-18 14:41:58 +0200

    TLS & exit
    added using TLS (plus certificate and key), and using the word "exit" to exit the chat

--- client.py
+++ client.py
@@ -2,38 +2,45 @@ import socket
 import threading
 import datetime
 import time
+import ssl
 
 #varialbes
 host = "127.0.0.1"
 port = 8000
 close_event = threading.Event()  # Event to signal when to close the connection
 
+#listening to the server
 def listen_for_msg_from_server(client):
-    while True:
+    #while connected
+    while not close_event.is_set():
         message = client.recv(2048).decode("UTF-8")
-        #print("got message! " + message)
+        #if message is empty
         if message != "":
+            #if the credentials are wrong
             if message == "BAD": 
                 print("Bad Credentials, Ended Connection")
-                close_event.set()  # Signal to close the connection
-                break
+                close_event.set()  #signal to close the connection
+                client.close()
+                break  #exit the function to stop receiving further messages
             else:
-                #print("printing the message!")
                 username = message.split("~ ")[0].split("| ")[1]
                 content = message.split("~ ")[1]
                 print(f"{datetime.datetime.now()} | {username} ~ {content}")
         else:
-            print("message from server is empty")
+            print("Ending Connection... ")
+            close_event.set()  #signal to close the connection
+            client.close()
+            break  #exit the function to stop receiving further messages
 
 def send_msg_to_server(client):
     while not close_event.is_set():
         time.sleep(0.05)
+        if close_event.is_set(): break
         message = input("Message: ")
         if message != "":
             client.sendall(message.encode("UTF-8"))
         else: 
             print("Message is empty")
-            send_msg_to_server(client)    
 
 def communicate_to_server(client):
     username = input("Enter username: ")
@@ -43,6 +50,7 @@ def communicate_to_server(client):
         client.sendall(f"{username}*****{password}".encode("UTF-8"))
     else:
         print("username or password can't be empty!")
+        client.close()
         exit(0)
     #listens then sends
     threading.Thread(target=listen_for_msg_from_server, args=(client, )).start()
@@ -56,16 +64,22 @@ def main():
     #create socket
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    #create context
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE  
+    secure_socket = context.wrap_socket(client, server_side=False, server_hostname="localhost")
+
     #try to connect to the server
     try: 
-        client.connect((host, port))
+        secure_socket.connect((host, port))
         print("Connected!")
 
     except: print(f"Unable to bind to host {host} in port {port}")
     
-    communicate_to_server(client)
+    communicate_to_server(secure_socket)
 
-    print("Thanks, See You Soon")
+    print("Thanks, See You Soon!")
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -3,16 +3,17 @@ import threading
 import datetime
 import os
 import hashlib
-
+import ssl
 
 #variables
-host = "127.0.0.1"
-port = 8000
+host = "127.0.0.1" #host
+port = 8000 #port
 listener_limit = 5
 active_client = [] #all currently connected users
-cred_path = r"C:\Users\kyoto\Documents\GitHub\FirstPython\chat\password.txt"
-chats_path = r"C:\Users\kyoto\Documents\GitHub\FirstPython\chat\chats.txt"
+cred_path = r"C:\Users\kyoto\Documents\GitHub\FirstPython\chat\password.txt" #path for credentials file
+chats_path = r"C:\Users\kyoto\Documents\GitHub\FirstPython\chat\chats.txt" #path for chat history file
 
+#generate salt for the password
 def generate_salt():
     #128 bit salt value
     salt = os.urandom(16)
@@ -23,20 +24,18 @@ def secure_password(password, salt):
    password_hash = hashlib.sha256(str(salt).encode() + str(password).encode())  #hash with salt
    return password_hash.digest() #returns bytes of salt and hashed password
 
-#check password
+#check username and password
 def check_cred(username, hashed_password):
     while True:
         with open(cred_path, "r") as accounts_file:
             for line in accounts_file:
                 file_username, file_password, file_salt = line.replace("\n","").split("*****")
-                #print(hashed_password, file_password.encode())
-                #print(hashed_password == file_password.encode())
-                print("filepassword type", type(file_password), "hashedpassword", type(hashed_password))
                 hashed_password = str(hashed_password)
                 if file_username == username and file_password == hashed_password:
                     return True
             return False
-        
+
+#check if username exists
 def check_username(username):
     while True:
         try:
@@ -49,6 +48,7 @@ def check_username(username):
         except:
             return False
 
+#get the salt that matches the username
 def get_salt(username):
     while True:
         with open(cred_path, "r") as accounts_file:
@@ -62,14 +62,18 @@ def get_salt(username):
 def listen_for_messages(client, username):
     while True:
         message = client.recv(2048).decode("UTF-8")
-
         #if message is empty
         if message != "": 
+            #if the clients quits
+            if message.lower() == "exit":
+                print(f"Ended Connection With {client.getpeername()[0]}:{client.getpeername()[1]}")
+                client.close()
+                exit(0)
+            #else, continues as usual
             final_msg = f"{datetime.datetime.now()} | {username} ~ {message}"
-            #print(final_msg)
             send_messages_to_all(final_msg)
-
-            with open(chats_path, 'a') as file:
+            #add to chat file
+            with open(chats_path, 'a', encoding='utf-8') as file:
                 file.write(final_msg + "\n")
 
         else:
@@ -83,7 +87,6 @@ def send_message_to_client(client, message):
 
 #send to all connected users
 def send_messages_to_all(message):
-    print()
     for user in active_client:
         send_message_to_client(user[1], message) #user[1] is the client object in the tuple
 
@@ -93,6 +96,7 @@ def client_handler(client):
     while True:
         cred = client.recv(2048).decode("UTF-8")
 
+        #dicided by *****
         username = cred.split("*****")[0]
         password = cred.split("*****")[1]
 
@@ -100,25 +104,27 @@ def client_handler(client):
         if check_username(username):
             #a salt exists, we'll get it and then compare
             salt = get_salt(username)
-            print("from get salt: ")
-            print(salt)
             hashed_password = secure_password(password, salt)
 
             #check username and password match
             if check_cred(username, hashed_password):
                 #add to active clients
                 active_client.append((username, client))
+                
                 #sends message
                 welcome_msg = f"{datetime.datetime.now()} | SERVER ~ {username} has entered the chat"
                 send_messages_to_all(welcome_msg)
+                
                 #add to file
-                with open(chats_path, 'a') as file:
+                with open(chats_path, 'a', encoding='utf-8') as file:
                     file.write(welcome_msg + "\n")
                 break
+            
             #username exists but password is wrong
             else:
                 print("Bad Password!")
                 send_message_to_client(client, "BAD")
+                client.close()
                 exit(0)
             
         else: #new username and password
@@ -128,7 +134,7 @@ def client_handler(client):
             welcome_msg = f"{datetime.datetime.now()} | SERVER ~ {username} signed up and has entered the chat"
             send_messages_to_all(welcome_msg)
             #add to file
-            with open(chats_path, 'a') as file:
+            with open(chats_path, 'a', encoding='utf-8') as file:
                 file.write(welcome_msg + "\n")
             salt = generate_salt()
             hashed_password = secure_password(password, salt)
@@ -140,22 +146,30 @@ def client_handler(client):
     threading.Thread(target=listen_for_messages, args=(client, username, )).start()
 
 def main():
-    print("go")
     #create socket server object and binding
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
+    #try binding
     try: 
         server.bind((host, port))
         print("The server is up!")
 
-    except: print(f"Unable to bind to host {host} in port {port}")
-    
+    except: 
+        print(f"Unable to bind to host {host} in port {port}")
+
     #to how many we listen
     server.listen(listener_limit)
+    
+    #create context to wrap the socket
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain(certfile="certificate.pem", keyfile="private_key.pem")
+
+    #encrypt the socket
+    secure_socket = context.wrap_socket(server, server_side=True)
 
     while True:
         #accept connection
-        client, address = server.accept()
+        client, address = secure_socket.accept()
         print(f"Accepted connection from {address[0]}:{address[1]}")
 
         #create and start thread every time a client connects
